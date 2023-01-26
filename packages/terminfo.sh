d2be083d87e30dfeab9bec62f66e3dedbf363513
Jiarong <alienover.l.d@gmail.com>
2023-01-26 11:28:54 -0800

    fix(htop): add terminal info for tmux-256color

---
+++ packages/terminfo.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+
+# Refer to https://gist.github.com/nicm/ea9cf3c93f22e0246ec858122d9abea1
+# INFO: Original solution
+# $ curl -O https://gist.githubusercontent.com/nicm/ea9cf3c93f22e0246ec858122d9abea1/raw/37ae29fc86e88b48dbc8a674478ad3e7a009f357/tmux-256color
+# $ /usr/bin/tic -x tmux-256color
+
+/usr/bin/tic -x ~/.terminfo/74/tmux-256color
--- setup.sh
+++ setup.sh
@@ -2,44 +2,46 @@
 CHECK_MARK="\033[0;32m\033[0m"
 
 DOT_DIR="$PWD/dotfiles"
-echo "HOME: ${HOME}"
+TARGET_DIR="$HOME"
+
+echo "TARGET DIR: ${TARGET_DIR}"
 echo "DOT DIR: $DOT_DIR"
 
 # Symlinks all the dotfiles into your home directory
 echo "\nSetting up symlinks to custom dotfiles ...\n"
 
 function create_link() {
-    local source="$1"
-    local target="$2"
-    local source_file=`echo $source | sed "s|$PWD|.|"`
-    local target_file=`echo $target | sed "s|$HOME|~|"`
+  local source="$1"
+  local target="$2"
+  local source_file=`echo $source | sed "s|$PWD|.|"`
+  local target_file=`echo $target | sed "s|$TARGET_DIR|~|"`
 
-    echo "\033[0;90m\c"
+  echo "\033[0;90m\c"
 
-    if [[ -e "$target" ]]; then
-        echo "$target_file already exists, renaming it to $target_file.bak \c"
+  if [[ -e "$target" ]]; then
+    echo "$target_file already exists, renaming it to $target_file.bak \c"
 
-        if [[ -e "$target.bak" ]]; then
-            rm -r "$target.bak"
-        fi
-
-	mv $target $target.bak
+    if [[ -e "$target.bak" ]]; then
+      rm -r "$target.bak"
     fi
-    ln -s $source $target
 
-    echo "\r\033[K\c" 
-    echo "$CHECK_MARK \033[0;90m `printf "%-35s->" $source_file` \t$target_file \033[0m"
+    mv $target $target.bak
+  fi
+  ln -s $source $target
+
+  echo "\r\033[K\c"
+  echo "$CHECK_MARK \033[0;90m `printf "%-35s->" $source_file` \t$target_file \033[0m"
 }
 
 for FILE in `ls -A $DOT_DIR`; do
-    if [[ $FILE == ".config" ]]; then
-        for FOLDER in `ls -A $DOT_DIR/$FILE`; do
-            if [[ -d "$DOT_DIR/$FILE/$FOLDER" ]]; then
-                create_link "$DOT_DIR/$FILE/$FOLDER" "$HOME/$FILE/$FOLDER"
-            fi
-        done
-    else
-        create_link "$DOT_DIR/$FILE" "$HOME/$FILE"
-    fi
+  if [[ $FILE == ".config" ]]; then
+    for FOLDER in `ls -A $DOT_DIR/$FILE`; do
+      if [[ -d "$DOT_DIR/$FILE/$FOLDER" ]]; then
+        create_link "$DOT_DIR/$FILE/$FOLDER" "$TARGET_DIR/$FILE/$FOLDER"
+      fi
+    done
+  else
+    create_link "$DOT_DIR/$FILE" "$TARGET_DIR/$FILE"
+  fi
 done
 echo "\nDone!"
