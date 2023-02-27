0dd1fca554f7f237c978240ae4f9f41526adc90a
Jiarong <alienover.l.d@gmail.com>
2023-02-26 22:27:15 -0800

    fix(nvim): use underline as the buffers tab bar

--- dotfiles/.config/nvim/lua/config/bufferline-config.lua
+++ dotfiles/.config/nvim/lua/config/bufferline-config.lua
@@ -4,11 +4,14 @@
 local config = {
   highlights = require("catppuccin.groups.integrations.bufferline").get(),
   options = {
+    indicator = {
+      style = "underline",
+    },
     show_close_icon = false,
     show_buffer_close_icons = false,
     diagnostics = "nvim_lsp",
     always_show_bufferline = true,
-    separator_style = "slant",
+    separator_style = { "", "" },
     diagnostics_indicator = function(count)
       return "(" .. count .. ")"
     end,
