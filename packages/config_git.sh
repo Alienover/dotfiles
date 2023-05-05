514c34c07f57b0b2f7ed8ecfbf06f41fac4954fd
Jiarong <alienover.l.d@gmail.com>
2023-05-05 11:38:42 -0700

    fix(git): script to setup git username, email, gpg key

---
+++ packages/config_git.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+USERNAME=""
+EMAIL=""
+REPO=""
+
+for arg in "$@"; do
+  case $arg in
+    -n=*|--name=*)
+      USERNAME="${arg#*=}"
+      shift
+      ;;
+    -e=*|--email=*)
+      EMAIL="${arg#*=}"
+      shift
+      ;;
+    -r=*|--repo=*)
+      REPO="${arg#*=}"
+      shift
+      ;;
+  esac
+done
+
+which git > /dev/null 2>&1 || exit 1
+
+which gpg > /dev/null 2>&1|| exit 1
+
+if [ -z "$USERNAME" ]; then
+  echo "Missing username... please include the username by --name={{ username }}"
+  exit 1
+fi
+
+if [ -z "$EMAIL" ]; then
+  echo "Missing email... please include the email by --email={{ email }}"
+  exit 1
+fi
+
+if [ -z "$REPO" ]; then
+  echo "Missing git repo... please include the git repo directory by --repo={{ repo path }}"
+  exit 1
+else
+  existed=$(cd "$REPO"; git status > /dev/null 2>&1; echo $?)
+  if [ "$existed" != 0 ]; then
+    echo "The specified directory is not a git repo."
+    exit 1
+  fi
+fi
+
+
+GPG_KEY=`gpg --list-keys | grep "$EMAIL" -C 1 | head -1 | xargs echo`
+
+if [ -z "$GPG_KEY" ]; then
+  echo "No GPG Key found for email [$EMAIL]. Please create a GPG key with this email first"
+  exit 1
+fi
+
+echo "Setting git config for username: [$USERNAME] email: [$EMAIL]\n"
+
+cd "$REPO"
+
+echo "Configuring $REPO\n"
+
+echo "Setting up user info"
+git config user.name "$USERNAME"
+git config user.email "$EMAIL"
+
+echo ""
+echo "Setting up GPG\n"
+git config gpg.program $(which gpg)
+git config user.signingkey $GPG_KEY
+git config --global commit.gpgsign true
+
+echo "Git config info"
+git config --local -l | grep 'user'
+
+echo ""
+echo "Finished $REPO"
+
+cd - > /dev/null
