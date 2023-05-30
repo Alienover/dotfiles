aadbbf909bf5434cc7e9ffcc3f13470d10ff948f
Jiarong <alienover.l.d@gmail.com>
2023-05-30 16:46:20 -0700

    fix(nvim): improve lazy.nvim bootstrap

--- dotfiles/.config/nvim/lua/utils/lazy.lua
+++ dotfiles/.config/nvim/lua/utils/lazy.lua
@@ -4,8 +4,9 @@ local consts = require("utils.constants")
 local icons = consts.icons
 
 local M = {}
+local scope = {}
 
-function M.bootstrap()
+function scope:bootstrap()
   local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
   if not vim.loop.fs_stat(lazypath) then
     vim.fn.system({
@@ -19,42 +20,51 @@ function M.bootstrap()
   end
   ---@diagnostic disable-next-line: undefined-field
   vim.opt.rtp:prepend(lazypath)
+
+  local found, lazy = pcall(require, "lazy")
+  if found then
+    self.lazy = lazy
+  end
 end
 
 function M.setup(plugins)
-  M.bootstrap()
+  scope:bootstrap()
 
-  local sizing = utils.get_float_win_sizing()
+  if scope.lazy then
+    local sizing = utils.get_float_win_sizing()
 
-  require("lazy").setup(plugins, {
-    defaults = {
-      lazy = true,
-    },
-    dev = {
-      path = consts.LOCAL_PLUGINS_FOLDER,
-    },
-    install = {
-      -- try to load one of these colorschemes when starting an installation during startup
-      colorscheme = { "catppuccin-mocha" },
-    },
-    ui = {
-      size = { width = sizing.width, height = sizing.height },
-      border = "rounded",
-      icons = {
-        cmd = icons.misc.Command,
-        config = icons.ui.Gear,
-        event = icons.kind.Event,
-        ft = icons.ui.NewFile,
-        init = icons.ui.BigUnfilledCircle,
-        keys = icons.misc.Keyboard,
-        plugin = icons.kind.Function,
-        runtime = icons.misc.Watch,
-        source = icons.ui.Note,
-        start = icons.ui.Circle,
-        task = icons.ui.Comment,
+    scope.lazy.setup(plugins, {
+      defaults = {
+        lazy = true,
       },
-    },
-  })
+      dev = {
+        path = consts.LOCAL_PLUGINS_FOLDER,
+      },
+      install = {
+        -- try to load one of these colorschemes when starting an installation during startup
+        colorscheme = { "catppuccin-mocha" },
+      },
+      ui = {
+        size = { width = sizing.width, height = sizing.height },
+        border = "rounded",
+        icons = {
+          cmd = icons.misc.Command,
+          config = icons.ui.Gear,
+          event = icons.kind.Event,
+          ft = icons.ui.NewFile,
+          init = icons.ui.BigUnfilledCircle,
+          keys = icons.misc.Keyboard,
+          plugin = icons.kind.Function,
+          runtime = icons.misc.Watch,
+          source = icons.ui.Note,
+          start = icons.ui.Circle,
+          task = icons.ui.Comment,
+        },
+      },
+    })
+  else
+    vim.notify("Failed to initialize lazy.nvim", vim.log.levels.ERROR)
+  end
 end
 
 return M
