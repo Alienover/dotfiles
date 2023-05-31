6df0418d36faf873fcdd3a1895c00e999e514dbf
Jiarong <alienover.l.d@gmail.com>
2023-05-31 16:32:12 -0700

    fix(nvim): move surround config to file

--- dotfiles/.config/nvim/lua/plugins/enhance.lua
+++ dotfiles/.config/nvim/lua/plugins/enhance.lua
@@ -18,24 +18,9 @@ return {
 
   {
     "echasnovski/mini.surround",
-    keys = { "s", "ds", "cs" },
+    keys = { "s", { "s", mode = "v" }, "ds", "cs" },
     config = function()
-      require("mini.surround").setup({
-        -- Module mappings. Use `''` (empty string) to disable one.
-        mappings = {
-          add = "s", -- Add surrounding in Normal and Visual modes
-          delete = "ds", -- Delete surrounding
-          replace = "cs", -- Replace surrounding
-
-          -- Disabled
-          find = "", -- Find surrounding (to the right)
-          find_left = "", -- Find surrounding (to the left)
-          highlight = "", -- Highlight surrounding
-          update_n_lines = "", -- Update `n_lines`
-          suffix_last = "", -- Suffix to search with "prev" method
-          suffix_next = "", -- Suffix to search with "next" method
-        },
-      })
+      require("config.surround-config")
     end,
   },
 
