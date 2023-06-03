9949bf18419edbfd2fdf907d1a6848783ea78ade
Jiarong <alienover.l.d@gmail.com>
2023-06-02 18:00:45 -0700

    fix(nvim): layout change for mergetool

--- dotfiles/.config/nvim/lua/config/diffview-config.lua
+++ dotfiles/.config/nvim/lua/config/diffview-config.lua
@@ -1,11 +1,12 @@
 local config = {
   default_args = {
     DiffviewOpen = { "--imply-local" },
+    DiffviewFileHistory = { "--base=LOCAL" },
   },
   view = {
     merge_tool = {
       -- Config for conflicted files in diff views during a merge or rebase.
-      layout = "diff3_mixed",
+      layout = "diff4_mixed",
       disable_diagnostics = true, -- Temporarily disable diagnostics for conflict buffers while in the view.
       winbar_info = true, -- See ':h diffview-config-view.x.winbar_info'
     },
