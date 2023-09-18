a09942b7e6eca821166968ed551dce3d37d33fea
Jiarong <alienover.l.d@gmail.com>
2023-09-18 14:47:12 -0700

    fix(brew): necessary env var due to brew new version

--- dotfiles/.config/zsh/plugins/brew/brew.zsh
+++ dotfiles/.config/zsh/plugins/brew/brew.zsh
@@ -3,17 +3,19 @@
 source "$ZDOTDIR/zsh-functions.sh"
 
 function __init_env {
-    local BREW_PREFIX="/opt/homebrew"
+  local BREW_PREFIX="/opt/homebrew"
 
-    local BREW_BIN="$BREW_PREFIX/bin"
-    local BREW_SBIN="$BREW_PREFIX/sbin"
+  local BREW_BIN="$BREW_PREFIX/bin"
+  local BREW_SBIN="$BREW_PREFIX/sbin"
 
-    export PATH="$BREW_BIN:$BREW_SBIN:$PATH"
+  export PATH="$BREW_BIN:$BREW_SBIN:$PATH"
+
+  export HOMEBREW_NO_INSTALL_FROM_API=1
 }
 
 # Brew
 function __load_brew {
-    eval "$(/opt/homebrew/bin/brew shellenv)"
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 }
 
 __init_env
