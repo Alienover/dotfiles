1261109d7775fed78a82e1aa32ded5aab1d1e420
Jiarong <alienover.l.d@gmail.com>
2024-04-17 14:23:41 -0700

    chore(zsh): remove perl pkg

--- dotfiles/.config/zsh/plugins/perl/perl.zsh
+++
@@ -1,9 +0,0 @@
-# Place your plugin content here
-source "$ZDOTDIR/zsh-functions.sh"
-
-# Perl
-function __load_perl {
-    eval "$(perl -I$HOME/perl5/lib/perl5 -Mlocal::lib=$HOME/perl5)"
-}
-
-zsh_lazy_load perl "__load_perl"
