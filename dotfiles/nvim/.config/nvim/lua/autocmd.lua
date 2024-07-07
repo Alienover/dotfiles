0c013a5ffa7ba3223f2015d1ad8384bc98354a0b
Jiarong <alienover.l.d@gmail.com>
2024-07-07 15:00:15 -0700

    fix(nvim): cleanup autocmds

--- dotfiles/nvim/.config/nvim/lua/autocmd.lua
+++ dotfiles/nvim/.config/nvim/lua/autocmd.lua
@@ -5,24 +5,8 @@ local nmap = utils.nmap
 
 local groups = {
   filetype = vim.api.nvim_create_augroup("FT", { clear = true }),
-  folding = vim.api.nvim_create_augroup("Folding", { clear = true }),
-  terminal = vim.api.nvim_create_augroup("Terminal", { clear = true }),
   linting = vim.api.nvim_create_augroup("Linting", { clear = true }),
 }
-
--- vim.api.nvim_create_autocmd("TermEnter", {
---   desc = "Set terminal filetype as default",
---
---   group = groups.terminal,
---   callback = function()
---     local bufnr = vim.api.nvim_get_current_buf()
---     local curr_ft = vim.api.nvim_get_option_value("filetype", { buf = bufnr })
---
---     if curr_ft == "" then
---       vim.cmd.set("filetype=terminal")
---     end
---   end,
--- })
 
 vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlight on yark",
@@ -38,9 +22,7 @@ vim.api.nvim_create_autocmd("FileType", {
   group = groups.filetype,
   pattern = consts.special_filetypes.close_by_q,
   callback = function()
-    nmap("q", function()
-      utils.cmd("close")
-    end, { silent = true, buffer = 0 })
+    nmap("q", ":close<CR>", { silent = true, buffer = 0 })
   end,
 })
 
