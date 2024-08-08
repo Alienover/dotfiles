98f17fa11220d1eff9931f3caefb703aa658eee0
Jiarong <alienover.l.d@gmail.com>
2024-08-08 13:57:58 -0700

    feat(nvim): add markview.nvim for markdown preview

--- dotfiles/nvim/.config/nvim/lua/plugins/editor.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/editor.lua
@@ -68,4 +68,17 @@ return {
     dependencies = "nvim-treesitter/nvim-treesitter",
     config = true,
   },
+  {
+    "OXY2DEV/markview.nvim",
+    ft = "markdown", -- If you decide to lazy-load anyway
+
+    dependencies = {
+      -- You will not need this if you installed the
+      -- parsers manually
+      -- Or if the parsers are in your $RUNTIMEPATH
+      "nvim-treesitter/nvim-treesitter",
+
+      "nvim-tree/nvim-web-devicons",
+    },
+  },
 }
