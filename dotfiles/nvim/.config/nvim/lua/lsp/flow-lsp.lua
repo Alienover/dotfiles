7389266793c61473eb161a764a84d16aeffcef62
Jiarong <alienover.l.d@gmail.com>
2024-08-22 17:37:28 -0700

    fix(nvim): find root dir by vim.fs.root

--- dotfiles/nvim/.config/nvim/lua/lsp/flow-lsp.lua
+++ dotfiles/nvim/.config/nvim/lua/lsp/flow-lsp.lua
@@ -1,13 +1,12 @@
 -- Reference
 -- https://raw.githubusercontent.com/neovim/nvim-lspconfig/master/lua/lspconfig/flow.lua
 
-local util = require("lspconfig/util")
-
 local M = {
-  cmd = {'npx', '--no-install', 'flow', 'lsp'},
+  cmd = { "npx", "--no-install", "flow", "lsp" },
   filetypes = { "javascript", "javascriptreact", "javascript.jsx" },
-  root_dir = util.root_pattern(".flowconfig"),
+  root_dir = function(startpath)
+    return vim.fs.root(startpath, ".flowconfig")
+  end,
 }
 
--- require "lspconfig".flow.setup(M)
 return M
