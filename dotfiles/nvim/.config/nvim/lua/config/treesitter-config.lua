0be4b535c7c5906a15a9e6858f55ad1026b1c611
Jiarong <alienover.l.d@gmail.com>
2024-08-30 15:30:57 -0700

    fix(nvim): keymaps for TS incremental selection

--- dotfiles/nvim/.config/nvim/lua/config/treesitter-config.lua
+++ dotfiles/nvim/.config/nvim/lua/config/treesitter-config.lua
@@ -44,10 +44,10 @@ require("nvim-treesitter.configs").setup({
   incremental_selection = {
     enable = true,
     keymaps = {
-      init_selection = "∑", -- <alt-w> maps in normal mode to init the node/scope selection
-      node_incremental = "∑", -- <alt-w> increment to the upper named parent
-      node_decremental = "ß", -- <alt-s> decrement to the previous node
-      scope_incremental = "å", -- <alt-a> increment to the upper scope
+      init_selection = "gnn", -- in normal mode, start incremental selection
+      node_incremental = ".", -- in visual mode, increment to the upper named parent
+      node_decremental = ",", -- in visual mode, decrement to the previous named node
+      scope_incremental = "grc", -- in visual mode, increment to the upper scope
     },
   },
   textobjects = {
