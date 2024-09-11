d5036bf1a618263e400d88accd4189b23ed63d2e
Jiarong <alienover.l.d@gmail.com>
2024-09-11 15:05:18 -0700

    fix(nvim): remove snippets utils funs

--- dotfiles/nvim/.config/nvim/snippets/all.lua
+++ dotfiles/nvim/.config/nvim/snippets/all.lua
@@ -1,7 +1,5 @@
 local ls = require("luasnip")
 
-local u = require("utils.snippets")
-
 local s = ls.snippet
 local sn = ls.snippet_node
 local t = ls.text_node
@@ -9,13 +7,22 @@ local i = ls.insert_node
 local d = ls.dynamic_node
 local f = ls.function_node
 
+local date = function(fmt)
+  local fn = function(_, _, arg)
+    return os.date(arg)
+  end
+
+  fmt = fmt or "%Y-%m-%d"
+  return f(fn, {}, { user_args = { fmt } })
+end
+
 return {
   s({
     trig = "date",
     name = "Date",
     dscr = "Date in the form of %Y-%m-%d",
   }, {
-    u.date(),
+    date(),
   }),
 
   s({
@@ -23,6 +30,6 @@ return {
     name = "Date",
     dscr = "Date in the form of %Y-%m-%d %H:%M:%S",
   }, {
-    u.date("%Y-%m-%d %H:%M:%S"),
+    date("%Y-%m-%d %H:%M:%S"),
   }),
 }
