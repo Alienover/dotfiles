003c6f2a7602c25276ef6c2a4154564c80d79d21
Jiarong <alienover.l.d@gmail.com>
2024-09-12 11:48:21 -0700

    fix(zsh): replace tab with spaces

--- dotfiles/zsh/.config/zsh/plugins/pyenv/pyenv.zsh
+++ dotfiles/zsh/.config/zsh/plugins/pyenv/pyenv.zsh
@@ -4,34 +4,34 @@ source "$ZDOTDIR/zsh-functions.sh"
 export VIRTUALENVWRAPPER_HOOK_DIR="$HOME/.virtualenvs"
 
 function __init_env {
-    # Pyenv Path
-    export PYENV_ROOT="$HOME/.pyenv"
+  # Pyenv Path
+  export PYENV_ROOT="$HOME/.pyenv"
 
-    local PYENV_BIN="$PYENV_ROOT/shims"
+  local PYENV_BIN="$PYENV_ROOT/shims"
 
-    export PATH="$PYENV_BIN:$PATH"
+  export PATH="$PYENV_BIN:$PATH"
 }
 
 # pyenv
 # Refer to: https://github.com/davidparsson/zsh-pyenv-lazy/blob/master/pyenv-lazy.plugin.zsh
 function __load_pyenv {
-    eval "$(command pyenv init -)"
-    if [[ -n "${ZSH_PYENV_LAZY_VIRTUALENV}" ]]; then
-	eval "$(command pyenv virtualenv-init -)"
-    fi
+  eval "$(command pyenv init -)"
+  if [[ -n "${ZSH_PYENV_LAZY_VIRTUALENV}" ]]; then
+    eval "$(command pyenv virtualenv-init -)"
+  fi
 }
 
 function __load_pyenv_completions {
-    source "$(brew --prefix)/opt/pyenv/completions/pyenv.zsh"
+  source "$(brew --prefix)/opt/pyenv/completions/pyenv.zsh"
 }
 
 # virtualenv
 function __load_virtualenv {
-    # For virtualenvwrapper
-    pyenv virtualenvwrapper_lazy
+  # For virtualenvwrapper
+  pyenv virtualenvwrapper_lazy
 
-    export PYENV_VIRTUALENVWRAPPER_PREFER_PYVENV="true"
-    export VIRTUAL_ENV_DISABLE_PROMPT='true'
+  export PYENV_VIRTUALENVWRAPPER_PREFER_PYVENV="true"
+  export VIRTUAL_ENV_DISABLE_PROMPT='true'
 }
 
 __init_env
