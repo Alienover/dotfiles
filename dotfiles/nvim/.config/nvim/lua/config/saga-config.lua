b38c4c46fd698b62537b480e2041299c703aeb60
Jiarong <alienover.l.d@gmail.com>
2024-09-28 22:29:14 -0700

    fix(nvim): use native diagnostics navigate

--- dotfiles/nvim/.config/nvim/lua/config/saga-config.lua
+++ dotfiles/nvim/.config/nvim/lua/config/saga-config.lua
@@ -6,72 +6,54 @@ local nmap = utils.nmap
 local window_specing = utils.get_window_default_spacing()
 
 local config = {
-  ui = {
-    -- currently only round theme
-    colors = require("catppuccin.groups.integrations.lsp_saga").custom_colors(),
-    kind = require("catppuccin.groups.integrations.lsp_saga").custom_kind(),
-    border = "rounded",
-  },
-  finder = {
-    max_height = 0.2,
-    keys = {
-      vsplit = "v",
-      toggle_or_open = "<CR>",
-    },
-  },
-  diagnostic = {
-    show_code_action = false,
-  },
-  lightbulb = {
-    enable = false,
-  },
-  code_action = {
-    extend_gitsigns = true,
-    num_shortcut = true,
-    keys = {
-      -- string |table type
-      quit = { "q", "<ESC>" },
-      exec = "<CR>",
-    },
-  },
-  symbol_in_winbar = {
-    enable = false,
-    hide_keyword = true,
-    show_file = false,
-    click_support = false,
-    color_mode = true,
-    separator = " " .. icons.get("extended", "arrowRight") .. " ",
-  },
-  floaterm = {
-    height = (1 - window_specing.t * 2),
-    width = (1 - window_specing.l * 2),
-  },
-  definition = {
-    keys = {
-      edit = "o",
-      split = "s",
-      vsplit = "v",
-    },
-  },
+	ui = {
+		-- currently only round theme
+		colors = require("catppuccin.groups.integrations.lsp_saga").custom_colors(),
+		kind = require("catppuccin.groups.integrations.lsp_saga").custom_kind(),
+		border = "rounded",
+	},
+	finder = {
+		max_height = 0.2,
+		keys = {
+			vsplit = "v",
+			toggle_or_open = "<CR>",
+		},
+	},
+	lightbulb = {
+		enable = false,
+	},
+	code_action = {
+		extend_gitsigns = false,
+		num_shortcut = true,
+		keys = {
+			-- string |table type
+			quit = { "q", "<ESC>" },
+			exec = "<CR>",
+		},
+	},
+	symbol_in_winbar = {
+		enable = false,
+		hide_keyword = true,
+		show_file = false,
+		click_support = false,
+		color_mode = true,
+		separator = " " .. icons.get("extended", "arrowRight") .. " ",
+	},
+	floaterm = {
+		height = (1 - window_specing.t * 2),
+		width = (1 - window_specing.l * 2),
+	},
+	definition = {
+		keys = {
+			edit = "o",
+			split = "s",
+			vsplit = "v",
+		},
+	},
 }
 
 require("lspsaga").setup(config)
 
-local function lspsaga(sub_cmd, opts)
-  return function()
-    local silent = (opts and opts.silent) or false
-
-    if silent then
-      vim.F.npcall(utils.cmd, "Lspsaga " .. sub_cmd)
-    else
-      vim.cmd("Lspsaga " .. sub_cmd)
-    end
-  end
-end
-
--- Diagnostics navigation
-nmap("[d", lspsaga("diagnostic_jump_prev"), "LspSaga: Previous Diagnostic")
-nmap("]d", lspsaga("diagnostic_jump_next"), "LspSaga: Next Diagnostic")
 -- Definition and references
-nmap("gh", lspsaga("finder"), "LspSaga: Finder")
-nmap("gp", lspsaga("peek_definition"), "LspSaga: [G]o [P]eek")
+nmap("gh", ":Lspsaga finder<CR>", "LspSaga: Finder")
+nmap("gp", ":Lspsaga peek_definition<CR>", "LspSaga: [G]o [P]eek")
--- dotfiles/nvim/.config/nvim/lua/lsp/init.lua
+++ dotfiles/nvim/.config/nvim/lua/lsp/init.lua
@@ -11,153 +11,161 @@ local ensure_externals = constants.ensure_externals
 
 -- Keymaps for LSP interfaces
 local lsp_keymaps = function(_, bufnr)
-  local function map(keys, fnc, desc)
-    local opts = { buffer = bufnr }
-    if desc then
-      opts.desc = "LSP: " .. desc
-    end
+	local function map(keys, fnc, desc)
+		local opts = { buffer = bufnr }
+		if desc then
+			opts.desc = "LSP: " .. desc
+		end
 
-    nmap(keys, fnc, opts)
-  end
+		nmap(keys, fnc, opts)
+	end
 
-  map("go", "<C-o>zz", "[G]o [O]riginal")
-  map("gd", vim.lsp.buf.definition, "[G]o [D]efinition")
-  map("gD", vim.lsp.buf.declaration, "[G]o [D]eclaration")
-  map("gi", vim.lsp.buf.implementation, "[G]o [I]mplementation")
-  map("gr", vim.lsp.buf.references, "[G]o [R]eferences")
+	map("go", "<C-o>zz", "[G]o [O]riginal")
+	map("gd", vim.lsp.buf.definition, "[G]o [D]efinition")
+	map("gD", vim.lsp.buf.declaration, "[G]o [D]eclaration")
+	map("gi", vim.lsp.buf.implementation, "[G]o [I]mplementation")
+	map("gr", vim.lsp.buf.references, "[G]o [R]eferences")
+	map("]d", function()
+		vim.diagnostic.goto_next({
+			float = {
+				border = "rounded",
+			},
+		})
+	end, "Next [D]iagnostic ")
+	map("[d]", function()
+		vim.diagnostic.goto_prev({
+			float = {
+				border = "rounded",
+			},
+		})
+	end, "Previous [D]iagnostic")
 end
 
 -- Custom on_attach handler
 local on_attach = function(client, bufnr)
-  lsp_keymaps(client, bufnr)
+	lsp_keymaps(client, bufnr)
 end
 
 ---@param filename string
 ---@return table
 local load_config = function(filename)
-  local config = {}
+	local config = {}
 
-  -- INFO: Load the config file if given
-  if (filename or "") ~= "" then
-    local success, module = pcall(require, filename)
-    if success then
-      config = module
-    end
-  end
+	-- INFO: Load the config file if given
+	if (filename or "") ~= "" then
+		local success, module = pcall(require, filename)
+		if success then
+			config = module
+		end
+	end
 
-  -- INFO: Capabilities config for nvim-cmp
-  local custom_capabilities = function()
-    local capabilities = require("cmp_nvim_lsp").default_capabilities(
-      vim.lsp.protocol.make_client_capabilities()
-    )
+	-- INFO: Capabilities config for nvim-cmp
+	local custom_capabilities = function()
+		local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-    capabilities.textDocument.completion.completionItem.snippetSupport = true
-    return capabilities
-  end
+		capabilities.textDocument.completion.completionItem.snippetSupport = true
+		return capabilities
+	end
 
-  local DEFAULT_CONFIG = {
-    -- INFO: disable on diff view by default
-    autostart = vim.o.diff == false,
+	local DEFAULT_CONFIG = {
+		-- INFO: disable on diff view by default
+		autostart = vim.o.diff == false,
 
-    on_attach = on_attach,
-    capabilities = custom_capabilities(),
-    flags = {
-      debounce_text_changes = 150,
-    },
-  }
+		on_attach = on_attach,
+		capabilities = custom_capabilities(),
+		flags = {
+			debounce_text_changes = 150,
+		},
+	}
 
-  return vim.tbl_deep_extend("force", DEFAULT_CONFIG, config)
+	return vim.tbl_deep_extend("force", DEFAULT_CONFIG, config)
 end
 
 -- Re-write lsp handlers
 local rewrite_lsp_handlers = function()
-  -- Automatically update diagnostics
-  vim.lsp.handlers["textDocument/publishDiagnostics"] =
-    vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-      virtual_text = {
-        prefix = " ",
-        spacing = 4,
-      },
-      signs = true,
-      underline = true,
-      severity_sort = true,
-      update_in_insert = false,
-    })
+	-- Automatically update diagnostics
+	vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+		virtual_text = {
+			prefix = " ",
+			spacing = 4,
+		},
+		signs = true,
+		underline = true,
+		severity_sort = true,
+		update_in_insert = false,
+	})
 
-  ---@diagnostic disable-next-line: duplicate-set-field
-  vim.lsp.handlers["textDocument/definition"] = function(...)
-    local status_ok, ts = pcall(require, "telescope.builtin")
-    if not status_ok then
-      return
-    end
+	---@diagnostic disable-next-line: duplicate-set-field
+	vim.lsp.handlers["textDocument/definition"] = function(...)
+		local status_ok, ts = pcall(require, "telescope.builtin")
+		if not status_ok then
+			return
+		end
 
-    return ts.lsp_definitions(...)
-  end
+		return ts.lsp_definitions(...)
+	end
 
-  ---@diagnostic disable-next-line: duplicate-set-field
-  vim.lsp.handlers["textDocument/references"] = function()
-    local status_ok, ts = pcall(require, "telescope.builtin")
-    if not status_ok then
-      return
-    end
+	---@diagnostic disable-next-line: duplicate-set-field
+	vim.lsp.handlers["textDocument/references"] = function()
+		local status_ok, ts = pcall(require, "telescope.builtin")
+		if not status_ok then
+			return
+		end
 
-    return ts.lsp_references({
-      bufnr = vim.api.nvim_get_current_buf(),
-      winnr = vim.api.nvim_get_current_win(),
-    })
-  end
+		return ts.lsp_references({
+			bufnr = vim.api.nvim_get_current_buf(),
+			winnr = vim.api.nvim_get_current_win(),
+		})
+	end
 end
 
 -- Re-write lsp diagnostic icons
 local rewrite_lsp_icons = function()
-  local signs = {
-    Error = icons.get("extended", "error"),
-    Warn = icons.get("extended", "warn"),
-    Hint = icons.get("extended", "hint"),
-    Info = icons.get("extended", "info"),
-  }
+	local signs = {
+		Error = icons.get("extended", "error"),
+		Warn = icons.get("extended", "warn"),
+		Hint = icons.get("extended", "hint"),
+		Info = icons.get("extended", "info"),
+	}
 
-  for type, icon in pairs(signs) do
-    local hl = "DiagnosticSign" .. type
-    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-  end
+	for type, icon in pairs(signs) do
+		local hl = "DiagnosticSign" .. type
+		vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+	end
 end
 
 -- Re-write lsp-config built-in functions
 local rewrite_lsp_cmds = function()
-  local window = require("lspconfig.ui.windows")
-  local og_percentage_range_window = window.percentage_range_window
+	local window = require("lspconfig.ui.windows")
+	local og_percentage_range_window = window.percentage_range_window
 
-  -- INFO: overwrite the function `percentage_range_window` from `nvim-lspconfig`
-  -- to create float window with customized config
-  ---@diagnostic disable-next-line: duplicate-set-field
-  window.percentage_range_window = function(...)
-    local win_info = og_percentage_range_window(...)
+	-- INFO: overwrite the function `percentage_range_window` from `nvim-lspconfig`
+	-- to create float window with customized config
+	---@diagnostic disable-next-line: duplicate-set-field
+	window.percentage_range_window = function(...)
+		local win_info = og_percentage_range_window(...)
 
-    vim.api.nvim_win_set_config(
-      win_info.win_id,
-      utils.get_float_win_opts({ border = true })
-    )
+		vim.api.nvim_win_set_config(win_info.win_id, utils.get_float_win_opts({ border = true }))
 
-    return win_info
-  end
+		return win_info
+	end
 end
 
 local function setup_lsp()
-  -- Setup the lsp for the one installed manually
-  for server, opts in pairs(ensure_externals) do
-    local is_lsp = opts.external_type == external_type.lsp
+	-- Setup the lsp for the one installed manually
+	for server, opts in pairs(ensure_externals) do
+		local is_lsp = opts.external_type == external_type.lsp
 
-    if is_lsp == true then
-      local config = load_config(opts.config_file)
+		if is_lsp == true then
+			local config = load_config(opts.config_file)
 
-      lspconfig[server].setup(config)
-    end
-  end
+			lspconfig[server].setup(config)
+		end
+	end
 
-  rewrite_lsp_handlers()
-  rewrite_lsp_icons()
-  rewrite_lsp_cmds()
+	rewrite_lsp_handlers()
+	rewrite_lsp_icons()
+	rewrite_lsp_cmds()
 end
 
 setup_lsp()
