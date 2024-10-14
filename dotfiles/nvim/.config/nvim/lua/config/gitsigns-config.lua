a0d3b1c96497b55a381eab0e844a1f23e6d96f2e
Jiarong <alienover.l.d@gmail.com>
2024-10-14 15:04:35 -0700

    fix(nvim): replace signs config for delete and topdelete

--- dotfiles/nvim/.config/nvim/lua/config/gitsigns-config.lua
+++ dotfiles/nvim/.config/nvim/lua/config/gitsigns-config.lua
@@ -7,8 +7,8 @@ local config = {
 	signs = {
 		add = { text = "▍" },
 		change = { text = "▍" },
-		delete = { text = "▸" },
-		topdelete = { text = "▾" },
+		delete = { text = " " },
+		topdelete = { text = " " },
 		changedelete = { text = "▍" },
 		untracked = { text = "▍" },
 	},
