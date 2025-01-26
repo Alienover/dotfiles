5124177bbb8aaf62e549eae8cc1a30a56a1126cb
Jiarong <alienover.l.d@gmail.com>
2025-01-26 20:29:54 +0800

    fix(tmux): better float window sizing

--- dotfiles/tmux/.config/tmux/sizing.sh
+++ dotfiles/tmux/.config/tmux/sizing.sh
@@ -4,7 +4,7 @@ get_window_width() {
   local scale="50%"
   local window_width=$(tmux display-message -p "#{window_width}")
 
-  if [[ $window_width -lt 250 ]]; then
+  if [[ $window_width -lt 260 ]]; then
     scale="80%"
   fi
 
@@ -16,7 +16,7 @@ get_window_height() {
   local scale="50%"
   local window_height=$(tmux display-message -p "#{window_height}")
 
-  if [[ $window_height -lt 65 ]]; then
+  if [[ $window_height -lt 70 ]]; then
     scale="80%"
   fi
 
