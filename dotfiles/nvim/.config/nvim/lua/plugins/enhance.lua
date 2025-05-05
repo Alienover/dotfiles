39e8c2479e9a2f178809d27e85b3746997551460
Jiarong Deng <alienover.l.d@gmail.com>
2025-05-04 22:48:09 -0700

    fix(ghostty,nvim): map macos option as alt

--- dotfiles/nvim/.config/nvim/lua/plugins/enhance.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/enhance.lua
@@ -199,20 +199,6 @@ return {
 	{
 		"echasnovski/mini.move",
 		event = "VeryLazy",
-		opts = {
-			mappings = {
-				-- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
-				left = "˙",
-				right = "¬",
-				down = "∆",
-				up = "˚",
-
-				-- Move current line in Normal mode
-				line_left = "˙",
-				line_right = "¬",
-				line_down = "∆",
-				line_up = "˚",
-			},
-		},
+		opts = {},
 	},
 }
