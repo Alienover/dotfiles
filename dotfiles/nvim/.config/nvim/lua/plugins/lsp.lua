52ee75487ef10ec9784955eb9e460cd2c6eb3085
Jiarong Deng <alienover.l.d@gmail.com>
2025-05-04 17:16:31 -0700

    fix(nvim): use lua_ls bundled luv library

--- dotfiles/nvim/.config/nvim/lua/plugins/lsp.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/lsp.lua
@@ -55,14 +55,9 @@ return {
 	{
 		"folke/lazydev.nvim",
 		ft = "lua", -- only load on lua files
-		dependencies = {
-			"Bilal2453/luvit-meta",
-		},
 		opts = {
 			library = {
-				-- See the configuration section for more details
-				-- Load luvit types when the `vim.uv` word is found
-				{ path = "luvit-meta/library", words = { "vim%.uv" } },
+				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
 			},
 		},
 	},
