d9838511b4a21c009767fdd0941c91bae1170b86
Jiarong Deng <alienover.l.d@gmail.com>
2025-09-09 10:42:48 -0700

    feat(nvim)!: remove the `update_n_lines` from nvim-surround refer to https://github.com/nvim-mini/mini.surround/commit/e170a2dde4b0a8905c9bb218ed428be94ec0b255

--- dotfiles/nvim/.config/nvim/lua/plugins/mini-surround.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/mini-surround.lua
@@ -21,7 +21,6 @@ return {
 				find = "", -- Find surrounding (to the right)
 				find_left = "", -- Find surrounding (to the left)
 				highlight = "", -- Highlight surrounding
-				update_n_lines = "", -- Update `n_lines`
 				suffix_last = "", -- Suffix to search with "prev" method
 				suffix_next = "", -- Suffix to search with "next" method
 			},
