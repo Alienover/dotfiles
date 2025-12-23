dac6f680aabdc8b3d425ed6072dbe213ac529b34
Jiarong Deng <alienover.l.d@gmail.com>
2025-12-23 14:20:43 -0800

    feat(nix): initialize devpod with nixpkgs

---
+++ dotfiles/nixpkgs/.config/nixpkgs/config.nix
@@ -0,0 +1,18 @@
+{
+  packageOverrides = pkgs: with pkgs; {
+    devPackages = pkgs.buildEnv {
+      name = "dev-packages";
+      paths = [
+        zsh
+        neovim
+        tmux
+        nodejs_24
+        fd
+        ripgrep
+        fzf
+        delta
+        git
+      ]
+    }
+  }
+}
