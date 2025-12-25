f09f0a14db3771394e9a6dc5e4e489672e38fb62
Jiarong Deng <alienover.l.d@gmail.com>
2025-12-24 17:13:05 -0800

    fix(nvim): only enable nvim-tmux-navigation under tmux

--- dotfiles/nvim/.config/nvim/lua/plugins/nvim-tmux-navigation.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/nvim-tmux-navigation.lua
@@ -3,6 +3,9 @@
 return {
 	{ -- Tmux Navigation
 		"alexghergh/nvim-tmux-navigation",
+		cond = function()
+			return vim.fn.getenv("TMUX") ~= vim.NIL
+		end,
 		keys = {
 			{ "<C-w>h", "<CMD>NvimTmuxNavigateLeft<CR>", mode = { "n", "t" }, desc = "Focus Left Pane" },
 			{ "<C-w>l", "<CMD>NvimTmuxNavigateRight<CR>", mode = { "n", "t" }, desc = "Focus Right Pane" },
