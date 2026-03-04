143cb47b13e24ad3a733d0513770ce9c3e48bc58
Jiarong Deng <alienover.l.d@gmail.com>
2026-03-04 15:02:53 -0800

    fix(nvim): fix the formatting

--- dotfiles/nvim/.config/nvim/lua/plugins/git.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/git.lua
@@ -4,7 +4,6 @@ return {
 		"lewis6991/gitsigns.nvim",
 		event = "User LazyPost",
 		opts = {
-
 			signs = {
 				add = { text = "▍" },
 				change = { text = "▍" },
@@ -15,21 +14,46 @@ return {
 			},
 			preview_config = { border = "rounded" },
 			on_attach = function(bufnr)
-					-- Hunk Navigation
-					vim.keymap.set("n", "]c", "<CMD>Gitsigns next_hunk<CR>zz", { buffer = bufnr, desc = "Next Hunk" })
-					vim.keymap.set("n", "[c", "<CMD>Gitsigns prev_hunk<CR>zz", { buffer = bufnr, desc = "Prev Hunk" })
+				-- Hunk Navigation
+				vim.keymap.set("n", "]c", "<CMD>Gitsigns next_hunk<CR>zz", { buffer = bufnr, desc = "Next Hunk" })
+				vim.keymap.set("n", "[c", "<CMD>Gitsigns prev_hunk<CR>zz", { buffer = bufnr, desc = "Prev Hunk" })
 
-					vim.keymap.set("n", "<space>gp", "<CMD>Gitsigns preview_hunk<CR>", { buffer = bufnr, desc = "[P]review hunk" })
-					vim.keymap.set("n", "<space>gb", "<CMD>Gitsigns blame_line<CR>", { buffer = bufnr, desc = "[B]lame line" })
+				vim.keymap.set(
+					"n",
+					"<space>gp",
+					"<CMD>Gitsigns preview_hunk<CR>",
+					{ buffer = bufnr, desc = "[P]review hunk" }
+				)
+				vim.keymap.set(
+					"n",
+					"<space>gb",
+					"<CMD>Gitsigns blame_line<CR>",
+					{ buffer = bufnr, desc = "[B]lame line" }
+				)
 
-					vim.keymap.set({ "n", "v" }, "<space>gs", ":Gitsigns stage_hunk<CR>", { buffer = bufnr, desc = "[S]tage hunk" })
-					vim.keymap.set({ "n", "v" }, "<space>gr", ":Gitsigns reset_hunk<CR>", { buffer = bufnr, desc = "[R]eset hunk" })
+				vim.keymap.set(
+					{ "n", "v" },
+					"<space>gs",
+					":Gitsigns stage_hunk<CR>",
+					{ buffer = bufnr, desc = "[S]tage hunk" }
+				)
+				vim.keymap.set(
+					{ "n", "v" },
+					"<space>gr",
+					":Gitsigns reset_hunk<CR>",
+					{ buffer = bufnr, desc = "[R]eset hunk" }
+				)
 
-					-- Text Objects
-					vim.keymap.set({ "o", "x" }, "ih", ":Gitsigns select_hunk<CR>", { buffer = bufnr })
+				-- Text Objects
+				vim.keymap.set({ "o", "x" }, "ih", ":Gitsigns select_hunk<CR>", { buffer = bufnr })
 
-					-- Blame
-					vim.keymap.set({ "n" }, "<space>gB", ":Gitsign blame<CR>", { buffer = bufnr, desc = "[Blame] current buffer" })
+				-- Blame
+				vim.keymap.set(
+					{ "n" },
+					"<space>gB",
+					":Gitsign blame<CR>",
+					{ buffer = bufnr, desc = "[Blame] current buffer" }
+				)
 			end,
 		},
 	},
