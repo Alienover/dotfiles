fff42bb54a4b5e5bb0cada095a6f67713bf1c3a4
Jiarong Deng <alienover.l.d@gmail.com>
2026-06-10 15:08:08 +0800

    fix(nvim): add prefix to kulala keymaps

--- dotfiles/nvim/.config/nvim/lua/plugins/kulala.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/kulala.lua
@@ -6,7 +6,7 @@ return {
 		opts = {
 			global_keymaps = true,
 			global_keymaps_prefix = "<leader>R",
-			kulala_keymaps_prefix = "",
+			kulala_keymaps_prefix = "<leader>",
 			lsp = { enable = false },
 			ui = {
 				max_response_size = 500 * 1024, -- 500 Kb
